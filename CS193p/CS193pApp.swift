a6cd83dc75a6296cb94de566504bbefa62385f09
Louis Tsai <louis993546@gmail.com>
2020-06-27 08:36:22 +0200

    Initial Commit

---
+++ CS193p/CS193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193pApp.swift
+//  CS193p
+//
+//  Created by Louis Tsai on 6/27/20.
+//
+
+import SwiftUI
+
+@main
+struct CS193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CS193p/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CS193p
+//
+//  Created by Louis Tsai on 6/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
